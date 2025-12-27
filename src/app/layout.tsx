82d574dc02aef205abff5f97d82f7053d6ef19b3
vesquicio <victosousa92@gmail.com>
2025-12-26 21:07:01 -0300

    update: ajustes na chamada das fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
-import { DreamOrphans, Dream_Orphans_Light, ElementaryGothic, Oskon } from './fonts';
+import { Oskon } from './fonts';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const minhaFonte = localFont({
-  src: [
-    {
-      path: '../../public/fonts/ZtBrosOskon90s-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  variable: '--zt-regular', // Opcional para usar com CSS variables
-})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${Oskon.variable} ${DreamOrphans.variable} ${ElementaryGothic.variable} ${Dream_Orphans_Light.variable}`}
+        className={`${Oskon.variable} `}
       >
         {children}
       </body>
