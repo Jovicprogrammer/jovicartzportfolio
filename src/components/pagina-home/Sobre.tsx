07ad47e5b232e7c2af340eb57963a41a3fd3b323
vesquicio <victosousa92@gmail.com>
2026-01-07 09:19:53 -0300

    update: sobre adicionado (não é mais lorem)

--- src/components/pagina-home/Sobre.tsx
+++ src/components/pagina-home/Sobre.tsx
@@ -9,7 +9,7 @@ export default function Sobre() {
         <h2 className="ml-10 text-7xl">Sobre</h2>
 
         <div className="text-right text-2xl mr-10">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vel sit dolorum reiciendis ab nulla officiis repudiandae delectus nisi quam, corrupti expedita alias esse placeat fuga ducimus non vitae! Mollitia? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vel sit dolorum reiciendis ab nulla officiis repudiandae delectus nisi quam, corrupti expedita alias esse placeat fuga ducimus non vitae! Mollitia? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vel sit dolorum reiciendis ab nulla officiis repudiandae delectus nisi quam, corrupti expedita alias esse placeat fuga ducimus non vitae! Mollitia?</p>
+            <p>ei, eai! me chamo vesquicio e este é meu sonho febril. desde muito pequeno sou apaixonado por fantasia e a ideia de criar histórias através da arte (e devaneios) sempre me fascinou. a essa altura da minha vida, ainda busco aquela magia da infância, exercitando minha criatividade através de desenhos, escrita, programação, edição de imagens e vídeos e o que mais cruzar o meu caminho. se curtir essa vibe - ou até mesmo se identificar com o que falei - talvez considere ficar por aqui. =) </p>
         </div>
 
 
